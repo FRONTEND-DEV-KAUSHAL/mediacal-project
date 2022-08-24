58739e88cfaf55247220e7394e1efb695d8d9ab4
= <=>
2022-08-24 14:23:10 +0530

    changes

--- src/common/api/Auth.api.js
+++ src/common/api/Auth.api.js
@@ -1,24 +1,31 @@
-import {createUserWithEmailAndPassword,  sendEmailVerification } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, sendEmailVerification } from "firebase/auth";
 import { auth } from "../../Fierbase";
 
 export const Signupapi = (data) => {
-    console.log("signupApi",data);  
+    console.log("signupApi", data);
 
     return new Promise((resolve, reject) => {
         createUserWithEmailAndPassword(auth, data.email, data.password)
-        .then((userCredential) => {
-            // Signed in 
-            const user = userCredential.user;
-            console.log(user);
-            sendEmailVerification(auth.currentUser)
-            .then(() => {
-                console.log("Auth kugfkus");
+            .then((userCredential) => {
+                const user = userCredential.user;
+                onAuthStateChanged(auth, (user) => {
+                    sendEmailVerification(auth.currentUser)
+                        .then(( ) => {
+                            resolve({payload:"Email sending To Your email"});
+                        })
+                        .catch((e) => {
+                            reject({payload:e})
+                        })
+                })
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                if (errorCode.localeCompare("auth/email-already-in-use") == 0) {
+                    reject({payload : "I think You used This Mail id To signup before now you have to login"});
+                } else {
+                    reject({payload:errorMessage});
+                }
             });
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // ..
-        });
     })
 }
--- src/redux/saga/Auth.saga.js
+++ src/redux/saga/Auth.saga.js
@@ -5,6 +5,7 @@ import * as ActionTypes from '../ActionTypes';
 function* SignUp(action) {
    try {
       const user = yield call(Signupapi, action.payload);
+      console.log(user);
     //   yield put({type: "USER_FETCH_SUCCEEDED", user: user});
    } catch (e) {
     //   yield put({type: "USER_FETCH_FAILED", message: e.message});
